4633ebf96d049373778d7296ff7cf4c99514102e
root <root@fedora.(none)>
2011-06-16 22:44:13 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectSql::Application.config.secret_token = 'a876627a0fb9ba5d866e2102af754eb922f86cfdb689453bb2b4411c30342cd6fe3ef4369e140fc4a9c437a977dd49831435136b34cd40ac6fbe7b11624de667'
