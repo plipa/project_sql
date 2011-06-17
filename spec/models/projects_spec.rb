5f8c4420d4e67060a090d0a3bbf36b07cc991a34
plipa <p.lipiak@gmail.com>
2011-06-17 11:03:01 +0200

    Testy poprawności nazwy projektu

--- spec/models/projects_spec.rb
+++ spec/models/projects_spec.rb
@@ -7,8 +7,19 @@ describe Projects do
     @test = {:name=>"Test title", :description => "Test description"}
   end
 
-  it "it should create new instance" do
+  it "should create new instance" do
     Projects.create!(@test)
+  end
+
+  it  "should require a name" do
+    no_name_project = Projects.new(@test.merge(:name=>""))
+    no_name_project.should_not be_valid
+  end
+
+  it "should require name <255 chars" do
+    @long = "a"*256
+    long_name = Projects.new(@test.merge(:name=>@long))
+    long_name.should_not be_valid
   end
 
 end
