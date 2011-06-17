3d6afb8db939254b84b561743904685370c870b9
plipa <p.lipiak@gmail.com>
2011-06-17 11:05:29 +0200

    Poprawiony model, na podstawie testów

--- app/models/projects.rb
+++ app/models/projects.rb
@@ -2,4 +2,6 @@ class Projects < ActiveRecord::Base
   attr_accessor  :name,  :description
 
   attr_accessible :name, :description
+   validates :name, :presence => true,
+                    :length => {:maximum => 255}
 end
